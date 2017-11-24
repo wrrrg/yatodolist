fcfa0f58c1d31e22f72b377a61b7bf9c3866dd94
BillMac <bill.gryta@gmail.com>
2017-11-24 09:48:19 -0600

    Added show/hide for new tasks

--- list.js
+++ list.js
@@ -9,15 +9,29 @@
 $("#view-current-link").on("click", function() {
     $("#todo-list-active").show();
     $("#todo-list-finished").hide();
+    $(".create-new-form").show();
+    $(".create-new-show").show();
 });
 
 
 $("#view-finished-link").click(function() {
   $("#todo-list-active").hide();
   $("#todo-list-finished").show();
+  $(".create-new-form").hide();
+  $(".create-new-show").hide();
 });
 
-$("#create-new-button").on("click", function() {
-  var newText = $("#create-new-field").val();
+$(".create-new-show").click(function() {
+  $(".create-new-form").toggle();
+  $(".create-new-toggle").toggle();
+});
+
+$(".create-new-hide").click(function() {
+  $(".create-new-form").toggle();
+  $(".create-new-toggle").toggle();
+});
+
+$(".create-new-button").on("click", function() {
+  var newText = $(".create-new-field").val();
   $("#todo-list-active").append("<li> " + newText + " </li>");
 });
